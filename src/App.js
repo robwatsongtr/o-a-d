9313cb42c76a0cf6aee60321d6475c6a7a514433
robwatsongtr <rwatso@gmail.com>
2022-05-29 20:40:49 -0700

    getting a simple users page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import * as React from "react";
-import { Admin } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-const App = () => <Admin dataProvider={dataProvider} />;
-
+// The App component renders an <Admin> component, which is the root component
+// of a react-admin app. This component expect a dataProvider prop.
+// The admin component expects one or more <Resource> child components.
+// Each resource maps a NAME to an ENDPOINT. 
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+)
 export default App;
